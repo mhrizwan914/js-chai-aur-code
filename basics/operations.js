3d6825188cd639f2ef956c2de2b933cdb299888d
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-06 22:53:36 +0000

    Learn Operations

---
+++ basics/operations.js
@@ -0,0 +1,34 @@
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(4 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 4);
+// console.log(2 % 4);
+
+let str1 = "Muhammad";
+let str2 = " Rizwan";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((((1 + 2) * 5) - 10) / 5);
+
+// console.log(+true);
+console.log(+"");
+
+let gameCounter = 1;
+gameCounter++;
+
+console.log(gameCounter);
+
+// Link to Study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
