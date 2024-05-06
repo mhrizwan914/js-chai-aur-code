c5d64e30c8ce640eabdc5707d7afb1ebe5ed910c
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-06 18:29:42 +0000

    Learn Conversions

---
+++ basics/conversions.js
@@ -0,0 +1,32 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let inNumber = Number(score);
+
+// console.log(typeof inNumber);
+// console.log(inNumber);
+
+//  "33" => 33
+// "33a" => NaN
+// true/false => 1/0
+// null => 0
+
+let name = undefined;
+
+let inBoolean = Boolean(name);
+
+// console.log(typeof inBoolean);
+// console.log(inBoolean);
+
+// 1/0 => true/false
+// null/undefined/"" => false
+// " " => true
+
+let number = null;
+
+let inString = String(number);
+
+console.log(typeof inString);
+console.log(inString);
