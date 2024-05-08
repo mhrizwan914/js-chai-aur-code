bf112b0c5fe79b055737f1208b4bb89039af5c3d
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-08 23:46:14 +0000

    Learn Strings

---
+++ basics/strings.js
@@ -0,0 +1,19 @@
+let myString = "Rizwan";
+// myString = new String("Rizwan");
+
+let myStringTwo = myString.toUpperCase();
+
+// console.log(`My name is ${myStringTwo} Khan`);
+
+let channelName = "chaiaurcode";
+
+// console.log(channelName[0]);
+// console.log(channelName.__proto__);
+// console.log(channelName.length);
+// console.log(channelName.length);
+// console.log(channelName.charAt(3));
+// console.log(channelName.indexOf("i"));
+// console.log(channelName.includes("i"));
+// console.log(channelName.replace("a", " "));
+console.log(channelName.substring(0, 5));
+console.log(channelName.slice(0, -4));
