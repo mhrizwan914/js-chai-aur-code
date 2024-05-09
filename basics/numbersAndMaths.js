034d91752a2f3b1d468c278f7b7535dcd88937f6
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-09 18:51:11 +0000

    Learn Numbers & Maths

---
+++ basics/numbersAndMaths.js
@@ -0,0 +1,26 @@
+let score = 100;
+// console.log(score);
+
+let balacne = new Number(100);
+// console.log(balacne);
+// console.log(balacne.valueOf());
+// console.log(new String(balacne.toString()).valueOf());
+// console.log(balacne.toFixed(2));
+
+let payment = 122.25;
+// console.log(payment.toPrecision(3));
+
+let loan = 1000000;
+// console.log(loan.toLocaleString('en-PK'));
+
+/* ================================= Math's ======================================== */
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.min(1, 9, 8, 2, 3));
+// console.log(Math.max(1, 9, 8, 2, 3));
+
+// console.log(Math.random());
+console.log(Math.floor((Math.random() * 10) + 1) + 6);
