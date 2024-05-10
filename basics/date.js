aeb8e5b0ff365a2f7c92f2b2ae483126a034a30d
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-10 18:57:33 +0000

    Learn Date

---
+++ basics/date.js
@@ -0,0 +1,24 @@
+let d = new Date();
+
+// console.log(typeof d);
+// console.log(d);
+// console.log(d.toDateString());
+// console.log(d.toJSON());
+// console.log(d.toISOString());
+// console.log(d.toLocaleDateString('en-PK'));
+// console.log(d.toString());
+// console.log(d.toTimeString());
+
+let specificDate = new Date(2023, 0, 25);
+specificDate = new Date(1999, 10, 22, 5, 30);
+specificDate = new Date("2022-12-13");
+
+// console.log(specificDate);
+// console.log(specificDate.toDateString());
+// console.log(specificDate.toLocaleString('en-PK'));
+
+let timeStamps = Date.now();
+
+console.log(timeStamps);
+console.log(specificDate.getTime());
+console.log(timeStamps / 1000);
