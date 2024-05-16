2463bb179afecdd6fc1bb6861640dbdd70bdf930
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-16 21:13:19 +0000

    Learn Object

--- intermediate/object.js
+++ intermediate/object.js
@@ -18,7 +18,20 @@ user.onLoggedOut = function () {
     console.log(`Good Bye, ${this.name}`);
 }
 
-console.log(user.name);
-console.log(user["Zip Code"]);
-console.log(user.onLoggedIn());
-console.log(user.onLoggedOut());
+// console.log(user.name);
+// console.log(user["Zip Code"]);
+// console.log(user.onLoggedIn());
+// console.log(user.onLoggedOut());
+
+const obj1 = {a: 1, b: 2};
+const obj2 = {b: 3, c: 4};
+
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign(obj1, obj2);
+// const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = {...obj1, ...obj2};
+
+console.log(Object.keys(obj3));
+console.log(Object.values(obj3));
+console.log(Object.entries(obj3));
+console.log(obj3.hasOwnProperty('c'));
