cd3379bb9feb5e624fcfb437d2c67e507ea0e82c
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-16 18:59:36 +0000

    Learn Object

---
+++ intermediate/object.js
@@ -0,0 +1,15 @@
+// Singleton
+// Object.create()
+
+// Object Literal
+const user = {
+    name: "Rizwan",
+    age: 18,
+    "Zip Code": "74500",
+    isLoggedIn: false,
+    onLoggedIn: function () {
+        return "Welcome Back!"
+    }
+}
+
+log
