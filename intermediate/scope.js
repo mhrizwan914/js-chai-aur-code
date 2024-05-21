21daeee0f3af0fca243046979a40b605ed0e071a
Muhammad Rizwan <104450999+mhrizwan914@users.noreply.github.com>
2024-05-21 20:41:21 +0000

    Learn Hoisting

--- intermediate/scope.js
+++ intermediate/scope.js
@@ -1,3 +1,4 @@
+// var c = "Rizwan";
 let a = 300;
 
 if (true) {
@@ -5,7 +6,36 @@ if (true) {
   const b = 20;
   var c = 30;
 }
+var c = "Rizwan";
 
 console.log(a);
 // console.log(b);
 console.log(c);
+
+function one() {
+  let user = "Hitesh";
+
+  function two() {
+    let website = "Youtube";
+    console.log(user);
+  }
+
+  // console.log(website);
+
+  two();
+}
+
+one();
+
+
+console.log(addOne(1));
+
+function addOne(num) {
+  return ++num
+}
+
+console.log(addTwo(1));
+
+const addTwo = function (num) {
+  return num + 2
+}
